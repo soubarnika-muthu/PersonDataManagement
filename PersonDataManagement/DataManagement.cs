ee2cb6b5f20a6786a3707d097434d4d7a8fe17d3
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-26 19:23:35 +0530

    [soubarnika]UC2-Retriving Top 2 Person Age Less Than 60 from list

--- PersonDataManagement/DataManagement.cs
+++ PersonDataManagement/DataManagement.cs
@@ -16,10 +16,17 @@ namespace PersonDataManagement
             personList.Add(new PersonData(2, "gem", "chennai", 29));
             personList.Add(new PersonData(6, "venkat", "trichy", 20));
             personList.Add(new PersonData(9, "shanthi", "madurai", 38));
-            personList.Add(new PersonData(5, "mani", "chennai", 19));
+            personList.Add(new PersonData(5, "mani", "chennai", 72));
             personList.Add(new PersonData(4, "muthu", "madurai", 28));
             personList.Add(new PersonData(7, "simar", "chennai", 64));
             DisplayList(personList);
+        }
+        public void RetriveTop2PersonAgeLessThan60()
+        {
+            //retrive the top two person detail from the list whose age is less tha 10;
+            List<PersonData> list = personList.FindAll(person => person.age < 60).OrderBy(x => x.age).Take(2).ToList();
+            Console.WriteLine("\nRetriving the top 2 person from list whose age less than 60 ");
+            DisplayList(list);
         }
         //method to display the list
         public void DisplayList(List<PersonData> list)
--- PersonDataManagement/Program.cs
+++ PersonDataManagement/Program.cs
@@ -10,6 +10,20 @@ namespace PersonDataManagement
             Console.WriteLine("Creating Person Data Management System");
             DataManagement management = new DataManagement();
             management.CreateList();
+            Console.WriteLine("1.RetriveTop2PersonAgeLessThan60");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch(choice)
+            {
+                case 1:
+                    management.RetriveTop2PersonAgeLessThan60();
+                    break;
+                case 0:
+                    break;
+                default:
+                    Console.WriteLine("Enter valid choice");
+                        break; ;
+
+            }
             Console.Read();
         }
     }
