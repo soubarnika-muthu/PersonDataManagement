c42b3500286a66d0c17b8ce4f00111e3e5779362
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-26 19:04:59 +0530

    [soubarnika]Example of Lambda Expression

---
+++ PersonDataManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonDataManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Implementaion of Lambda expression");
+        }
+    }
+}
